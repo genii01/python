5b77c81fed495e772ba269bc4f583d0532633235
genii01 <genii01@naver.com>
2021-10-19 01:00:56 +0900

    added

---
+++ underbar_func.py
@@ -0,0 +1,5 @@
+from underbar_func_util import *
+
+if __name__ == "__main__":
+    # func()
+    _underbarfunc()
---
+++ underbar_func_util.py
@@ -0,0 +1,5 @@
+def _underbarfunc():
+    print('underbar function')
+
+def func():
+    print('general function')
