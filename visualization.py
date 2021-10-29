bde743978ab1a1213dda08d6570378743194d90a
genii01 <genii01@naver.com>
2021-10-29 23:14:24 +0900

    add visualization

--- visualization.py
+++ visualization.py
@@ -1,37 +1,37 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-num_columns = app_train.dtypes[app_train.dtypes != 'object'].index
-cat_columns = app_train.dtypes[app_train.dtypes == 'object'].index
-
 def show_hist_by_target(df, cols):
-  cond_1 = (df['TARGET'] == 1)
-  cond_0 = (df['TARGET'] == 0)
+  cond_h = (df['Class'] == 'H')
+  cond_m = (df['Class'] == 'M')
+  cond_l = (df['Class'] == 'L')
+
   for col in cols:
-    fig, axs = plt.subplots(figsize = (12,4), nrows = 1, ncols = 3, squeeze = False)
-    sns.boxplot(data = df, x = 'TARGET', y = col, ax = axs[0][0])
-    sns.violinplot(data=df, x= 'TARGET', y= col, ax = axs[0][1])
-    sns.distplot(df.loc[cond_1,[col]], ax = axs[0][2], bins = 25, label = 1, color = 'r')
-    sns.distplot(df.loc[cond_0,[col]], ax = axs[0][2], bins = 25, label = 0, color = 'b')
+    fig, axs = plt.subplots(nrows = 1, ncols = 3, figsize = (14,7), squeeze = False)
+    sns.set_palette('RdYlBu')
+    sns.boxplot(data = df, x = 'Class', y = col, ax = axs[0][0])
+    sns.violinplot(data = df, x = 'Class', y = col, ax = axs[0][1])
+    sns.distplot(df[cond_h][col], bins = 50, ax = axs[0][2], label = 'H', color = 'red')
+    sns.distplot(df[cond_m][col], bins = 50, ax = axs[0][2], label = 'M', color = 'orange')
+    sns.distplot(df[cond_l][col], bins = 50, ax = axs[0][2], label = 'L', color = 'green')
+    plt.title('{} - distplotting'.format(col))
+    plt.show()
+
+show_hist_by_target(df, num_cols)
+
+def show_count_by_target(df, cols):
+  cond_h = (df['Class'] == 'H')
+  cond_m = (df['Class'] == 'M')
+  cond_l = (df['Class'] == 'L')
+
+  for col in cols:
+    fig, axs = plt.subplots(figsize = (14,7), nrows =1, ncols = 3, squeeze = False)
+    sns.set_palette('RdYlBu')
+    chart_h = sns.countplot(df[cond_h][col], ax = axs[0][0], order = df[col].value_counts().index)
+    chart_h.set_xticklabels(chart_h.get_xticklabels(), rotation = 45)
+    chart_m = sns.countplot(df[cond_m][col], ax = axs[0][1], order = df[col].value_counts().index)
+    chart_m.set_xticklabels(chart_m.get_xticklabels(), rotation = 45)
+    chart_l = sns.countplot(df[cond_l][col], ax = axs[0][2], order = df[col].value_counts().index)
+    chart_l.set_xticklabels(chart_l.get_xticklabels(), rotation = 45)
+    plt.title('{}-count_plotting'.format(col))
     plt.legend()
     plt.show()
 
-def show_count_by_target(df, columns):
-    cond_1 = (df['TARGET'] == 1)
-    cond_0 = (df['TARGET'] == 0)
-    
-    for column in columns:
-        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(18, 4), squeeze=False)
-        # countplot을 이용하여 category값의 histogram 표현
-        chart0 = sns.countplot(df[cond_0][column], ax=axs[0][0])
-        # x축의 tick label들이 값 유형이 많으므로 45도로 회전하여 표현
-        chart0.set_xticklabels(chart0.get_xticklabels(), rotation=45)
-        chart1 = sns.countplot(df[cond_1][column], ax=axs[0][1])
-        chart1.set_xticklabels(chart1.get_xticklabels(), rotation=45)
-        plt.legend()
-        plt.show()
-
-        
-show_count_by_target(app_train, cat_columns)
-
-show_hist_by_target(app_train,num_columns)
+show_count_by_target(df, cat_cols)
