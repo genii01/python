2226e8bb02c49c8132312d87a2d5f43be52453f1
genii01 <genii01@naver.com>
2021-10-31 20:58:32 +0900

    added

---
+++ min.py
@@ -0,0 +1,28 @@
+import logging
+from time import time, sleep
+# 로그 생성
+logger = logging.getLogger()
+
+# 로그의 출력 기준 설정
+logger.setLevel(logging.INFO)
+
+# log 출력 형식
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# log 출력
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+# log를 파일에 출력
+file_handler = logging.FileHandler('my.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+for i in range(10):
+	logger.info(f'{i}번째 방문입니다.')
+
+s = time()
+sleep(3)
+res = time() -s 
+logger.info(res)
